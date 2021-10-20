8c018a81e14b424b4d52c8c28caf8a528f9f9164
drodrigues21 <davidvilar21@gmail.com>
2021-10-20 17:42:11 +0900

    first commit

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'By8KV9SgiuU6X0suXhM2GwXEOTWx8S9A';
 
 // Get city weather
 const getWeather = async (cityId) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${cityId}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -13,7 +13,7 @@ const getWeather = async (cityId) => {
 
 // Get city information
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
